85e03a4133bd69eb2afed3340878a61fc00165ca
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-18 16:50:47 +0100

    ADD: counter

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const express = require('express')
+const app = express()
+const port = 3000
+const post = require('./post.js')
+
+app.get('/', (req, res) => {
+    res.send(post)
+})
+
+app.listen(port, () => {
+    console.log(`Server in ascolto sulla porta ${port}`);
+})
---
+++ post.js
@@ -0,0 +1,64 @@
+const { count } = require("console")
+
+let post = [
+    {
+        title: 'Ciambellone',
+        content:'lorem ipsnum dolor amet',
+        image: '/images/ciambellone.jpeg',
+        tags:[
+            'lorem',
+            'ipsum',
+            'dolor'
+        ]
+    },
+    {
+        title: 'Cracker barbabietola',
+        content:'lorem ipsnum dolor amet',
+        image: '/images/ciambellone.jpeg',
+        tags:[
+            'lorem',
+            'ipsum',
+            'dolor'
+        ]
+    },
+    {
+        title: 'Pane fritto dolce',
+        content:'lorem ipsnum dolor amet',
+        image: '/images/ciambellone.jpeg',
+        tags:[
+            'lorem',
+            'ipsum',
+            'dolor'
+        ]
+    },
+    {
+        title: 'Pasta barbabietola',
+        content:'lorem ipsnum dolor amet',
+        image: '/images/ciambellone.jpeg',
+        tags:[
+            'lorem',
+            'ipsum',
+            'dolor'
+        ]
+    },
+    {
+        title: 'Torta paesana',
+        content:'lorem ipsnum dolor amet',
+        image: '/images/ciambellone.jpeg',
+        tags:[
+            'lorem',
+            'ipsum',
+            'dolor'
+        ]
+    }
+]
+
+post.forEach((el, index) => {
+    el.id = index + 1
+})
+
+
+console.log(post);
+
+
+module.exports = post
